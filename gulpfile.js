10ebf36cd903d9727f924e6d6e61afcb70befba6
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-07-12 02:15:37 +0300

    Few more tasks for building css, js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 'use strict';
 
+var browserify = require('browserify');
+var concat = require('gulp-concat');
 var gulp = require('gulp');
+var source = require('vinyl-source-stream');
 
 /**
  * Starts the development server with live reload.
@@ -9,7 +12,23 @@ gulp.task('server', function () {
   process.env.WORKERS = 1;
   var cluster = require('./app');
 
-  gulp.watch(['app/@(controllers|modules|routes)/*.js', 'app/*.js'], function () {
+  gulp.watch(['bro/*/*.css'], ['css']);
+  gulp.watch(['bro/*/*.js', 'pages/*/*.js'], ['js']);
+  gulp.watch(['app/@(controllers|modules|routes)/*.js', 'app/*.js', 'bro/*/*.js'], function () {
     cluster.reload();
   });
+});
+
+gulp.task('css', function () {
+  gulp.src('bro/*/*.css')
+    .pipe(concat('index/index.css'))
+    .pipe(gulp.dest('static'));
+});
+
+gulp.task('js', function () {
+  browserify('pages/index/index.js')
+    .transform('babelify')
+    .bundle()
+    .pipe(source('index/index.js'))
+    .pipe(gulp.dest('static'));
 });
