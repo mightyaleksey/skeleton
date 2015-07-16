4d853e40bcafaea5f280cd5c38c31172796999a5
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-07-16 22:21:25 +0300

    Implemented livereload with saving the current state

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,25 @@
 'use strict';
 
+import lrApi from 'livereactload-api';
+import { comp, toClj } from 'mori';
 import { createRedux } from 'redux';
-import { toClj } from 'mori';
 import * as stores from '../../bro/stores';
 
 import React from 'react';
 import App from '../../bro/components/App';
 
-const initialState = window.STATE_FROM_SERVER;
-initialState.default = toClj(initialState.default);
-const redux = createRedux(stores, initialState);
+function initialize() {
+  const initialState = lrApi.getState() || transform(window.STATE_FROM_SERVER);
+  const redux = createRedux(stores, initialState);
+  redux.subscribe(comp(lrApi.setState, redux.getState));
 
-React.render(<App redux={redux} />, document.querySelector('body'));
+  React.render(<App redux={ redux } />, document.querySelector('body'));
+}
+
+function transform(state) {
+  state.default = toClj(state.default);
+  return state;
+}
+
+lrApi.onReload(initialize);
+document.addEventListener('DOMContentLoaded', initialize);
